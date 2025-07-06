e689fcd3eccd01f55be07a93465f5d98ff551157
Katie Chickering <katiechickering@Katies-MacBook-Air.local>
2025-07-06 01:02:47 -0700

    Fixed but in server file. Completed login functionality and styling in front end. Completed Header file.

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,12 +1,17 @@
 import { login } from "../services/user.service"
 import { useNavigate } from "react-router-dom"
 import { useLogin } from '../context/UserContext'
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { toast } from "react-toastify"
 
 export const LoginForm = () => {
     const navigate = useNavigate()
-    const { login:loginUser, isLoggedIn } = useLogin()
+    const { login:loginUser } = useLogin()
     const [ error, setError ] = useState('')
+
+    useEffect(() => {
+        if (error) toast.error(error)
+    }, [error])
 
     const handleSubmit = e => {
         e.preventDefault()
@@ -14,30 +19,52 @@ export const LoginForm = () => {
         login( { userName: userName.value, password: password.value } )
             .then( ()=> {
                 loginUser()
+                toast.success("Login sussessful!")
                 navigate('/')  
             } )
-            .catch( error => setError( error ) )
+            .catch( error => setError(error))
     }
 
     return(
-        <form onSubmit={handleSubmit}>
-            <h2>Login:</h2>
-            <label>
-                User Name:
-                <input 
-                    type="text" 
-                    name="userName" 
-                />
-            </label>
-            <label>
-                Password:
-                <input 
-                    type="password" 
-                    name="password" 
-                />
-            </label>
-            { error && <p>{error}</p>}
-            <input type="submit" value="Login" />
-        </form>
+        <div className="backgroundLayout items-center flex flex-col">
+
+            <div className="w-1/2">
+                <p className="text-center text-xl font-bold mb-8">
+                    Welcome to the Closet Organizer app! Don't have an account yet?
+                    Click the Register button above to sign up.
+                </p>
+            </div>
+
+            <form onSubmit={handleSubmit} className="border-2 border-brandNavy bg-brandBlue text-brandNavy p-10">
+                <p className="text-center text-4xl mb-8 text-white">Login</p>
+
+                <div className="mb-5">
+                    <label htmlFor="usertName" className="text-white">User Name:</label>
+                        <input
+                            type="text"
+                            name="userName"
+                            id="userName"
+                            required
+                        />
+                </div>
+
+                <div className="mb-8">
+                    <label htmlFor="password" className="text-white">Password:</label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            required
+                        />
+                </div>
+
+                {error && <p className="text-red-500 text-center mb-5">{error}</p>}
+                <div className="flex justify-center w-full">
+                    <button type="submit">Login</button>
+                </div>
+
+            </form>
+
+        </div>
     )
 }
--- client/src/components/RegistrationForm.jsx
+++ client/src/components/RegistrationForm.jsx
@@ -2,55 +2,145 @@ import { useNavigate } from 'react-router-dom'
 import { register } from '../services/user.service'
 import { useState } from 'react'
 import { useLogin } from '../context/UserContext'
+import { toast } from "react-toastify"
 
+const DEFAULT_FORM_VALUES = {
+    userName: "",
+    password: "",
+    confirmPassword: ""
+}
 
 export const RegistrationForm = () => {
     const navigate = useNavigate()
     const { login } = useLogin()
-    const [ errors, setErrors ] = useState({})
+    const [ dataErrors, setDataErrors ] = useState({})
+    const [formData, setFormData] = useState(DEFAULT_FORM_VALUES)
+    const [formErrors, setFormErrors] = useState({})
 
+    // Dynamically set form data
+    const handleChange = e => {
+        const {name, value} = e.target
+        setFormData(prev => ({...prev, [name]: value}))
+        validateData(name, value)
+    }
+
+    // Validate form inputs dynamically
+    const validateData = (name, value) => {
+        const validations = {
+            userName: () => false,
+            password: value => {
+                if (value.length < 8) return "Passwords must be at least eight characters long."
+                else if (value != formData.confirmPassword) return "Passwords must match."
+                else {
+                    setFormErrors(prev => ({...prev, confirmPassword: false}))
+                    return false
+                }
+            },
+            confirmPassword: value => {
+                if (value.length < 8) return "Passwords must be at least eight characters long."
+                else if (value != formData.password) return "Passwords must match."
+                else {
+                    setFormErrors(prev => ({...prev, password: false}))
+                    return false
+                }
+            }
+        }
+        setFormErrors(prev => ({...prev, [name]: validations[name](value)}))
+    }
+
+    // Check for errors before submitting form
+    const isReadyToSubmit = () => {
+        for (let key in formErrors){
+            if (formErrors[key] != false) {
+                return false
+            }
+        }
+        return true
+    }
+
+    // Submit form
     const handleSubmit = e => {
         e.preventDefault()
-        let { userName, password, confirmPassword } = e.target
-        userName = userName.value 
-        confirmPassword = confirmPassword.value 
-        password = password.value 
-        register({userName, password, confirmPassword})
-            .then( ()=>{ 
-                login()
-                navigate('/') 
-            })
-            .catch( errors => setErrors( errors ) )
+        if (!isReadyToSubmit()){
+            toast.error("Please make corrections to the form.")
+        }
+        else {
+            let { userName, password, confirmPassword } = e.target
+            userName = userName.value 
+            confirmPassword = confirmPassword.value 
+            password = password.value 
+            register({userName, password, confirmPassword})
+                .then( ()=>{ 
+                    login()
+                    toast.success("Account created successfully!")
+                    navigate('/') 
+                })
+                .catch( error => {
+                    console.log("registerAccount error:", error)
+                    setDataErrors(prev => ({...prev, createRequest: "Unable to create account."}))
+                    toast.error("Unable to create account.")
+                }
+            )
+        }
     }
 
     return(
-        <form onSubmit={handleSubmit}>
-            <h2>Register</h2>
-            <label>
-                User Name: 
-                <input 
-                    type="text" 
-                    name="userName"  
-                />
-                { errors.userName && <p>{errors.userName.message}</p> }
-            </label>
-            <label>
-                Password: 
-                <input 
-                    type="password" 
-                    name="password"  
-                />
-                { errors.password && <p>{errors.password.message}</p> }
-            </label>
-            <label>
-                Confirm Password: 
-                <input 
-                    type="password" 
-                    name="confirmPassword"  
-                />
-                { errors.confirmPassword && <p>{errors.confirmPassword.message}</p> }
-            </label>
-            <input type="submit" value="Register" />
-        </form>
+        <div className="backgroundLayout items-center flex flex-col">
+
+            <form onSubmit={handleSubmit} className="border-2 border-brandNavy bg-brandBlue text-brandNavy p-10">
+                <p className="text-center text-4xl mb-8 text-white">Register</p>
+
+                <div className="mb-5">
+                    <label htmlFor="usertName" className="text-white">User Name:</label>
+                        <input
+                            type="text"
+                            value={formData.userName}
+                            onChange={handleChange}
+                            name="userName"
+                            id="userName"
+                            required
+                        />
+                </div>
+
+                <div className="mb-5">
+                    {formErrors.password && <p className="text-red-500 text-center">{formErrors.password}</p>}
+                    <label htmlFor="password" className="text-white">Password:</label>
+                        <input
+                            type="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            name="password"
+                            id="password"
+                            required
+                            className={formErrors.confirmPassword ? "error" : ""}
+                        />
+                </div>
+
+                <div className="mb-8">
+                    {formErrors.confirmPassword && <p className="text-red-500 text-center">{formErrors.confirmPassword}</p>}
+                    <label htmlFor="confirmPassword" className="text-white">Confirm Password:</label>
+                    <input
+                        type="password"
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                        name="confirmPassword"
+                        id="confirmPassword"
+                        required
+                        className={formErrors.confirmPassword ? "error" : ""}
+                    />
+                </div>
+
+                {dataErrors.createRequest &&
+                    <p className="text-red-500 text-center">
+                        {dataErrors.createRequest}
+                    </p>
+                }
+
+                <div className="flex justify-center w-full">
+                    <button type="submit">Register</button>
+                </div>
+
+            </form>
+        </div>
     )
 }
