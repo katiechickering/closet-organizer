1d5771a3f9834a15f9f4e4f78c31d1c03a5d63f5
theclassic2 <clearvisionary@gmail.com>
2025-07-08 16:23:29 -0400

    7/8/25 update at 4:23pm ET

--- client/src/components/ViewClothingItem.jsx
+++ client/src/components/ViewClothingItem.jsx
@@ -46,7 +46,7 @@ export const ViewClothingItem = ({ setHeaderInfo }) => {
     }
 
     return (
-        <div>
+        <div className="backgroundLayout">
 
             {loading.clothingItem && <p className="text-center">{loading.clothingItem}</p>}
             {apiErrors.getRequest &&
@@ -55,13 +55,13 @@ export const ViewClothingItem = ({ setHeaderInfo }) => {
                 </p>
             }
 
-            <div className="flex flex-col justify-center lg:flex-row gap-30 items-start">
+            <div className="flex justify-center lg:flex-row gap-20">
 
-                <div className="w-2xl my-10 h-125 border-2 border-gray-300 rounded-lg flex items-center justify-center">
+                <div className="bg-white w-2xl my-10 h-125 border-2 border-gray-600 rounded-lg flex items-center justify-center">
                     <img className="h-123 m-w-2xl" src={clothingItem.image} alt={clothingItem.name} />
                 </div>
 
-                <div className="items-center text-2xl leading-relaxed text-center w-2xl my-10 border h-125 border-gray-300 p-4 rounded-md">
+                <div className="items-center text-2xl leading-relaxed text-center w-2xl my-10 h-125 border-2 border-gray-600 p-4 rounded-md bg-brandBlue text-white">
                     <p><strong>Name:</strong> {clothingItem.name}</p>
                     <p><strong>Category:</strong> {clothingItem.category}</p>
                     <p><strong>Size:</strong> {clothingItem.size}</p>
@@ -75,9 +75,9 @@ export const ViewClothingItem = ({ setHeaderInfo }) => {
 
             {apiErrors.deleteRequest && <p className="text-red-500 text-center">{apiErrors.deleteRequest}</p>}
             <div className="m-10 flex justify-center gap-4">
-                <Link to={`/clothingItem/update/${id}`} className="px-4 py-2 bg-gray-100 rounded shadow hover:bg-gray-200">Update Item</Link>
-                <button className="px-4 py-2 bg-red-100 text-red-700 rounded shadow hover:bg-red-200" onClick={deleteClothingItem}>Delete Item</button>
-            </div>
+                <Link to={`/clothingItem/update/${id}`} className="m-1 px-4 py-2 text-white bg-brandMauve rounded shadow hover:bg-brandNavy">Update Item</Link>
+                <button className="m-1 px-4 py-2 bg-brandMauve text-white rounded shadow hover:bg-red-700" onClick={deleteClothingItem}>Delete Item</button>
+            </div>  
 
         </div>
     )
